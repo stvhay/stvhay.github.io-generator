1b87c4b499849a8301bfb43e2ab9f79c632db8d3
Steve Hay <github@stevenhay.com>
2025-03-30 19:01:38 -0400

    fixed argument handling [no ci]
    Signed-off-by: Steve Hay <github@stevenhay.com>

--- utilities/publish.sh
+++ utilities/publish.sh
@@ -4,16 +4,13 @@ publish()
 pushd "${hugo_repo_dir:?}"  || return
 
     msg_arg=("-m")
-    case $# in
-        0) : ;;
-        1) msg_arg+=("$1") ;;
-        *)
-            >&2 echo "Usage:"
-            >&2 echo "    $0 commit_message"
-            exit 1
-        ;;
-    esac
-
+    if [[ $# -ne 1 ]]
+    then
+        >&2 echo "Usage:"
+        >&2 echo "    $0 commit_message"
+        exit 1
+    fi
+    msg_arg+=("$1")
     git -C public commit -s "${msg_arg[@]}"
     git -C public push
     git add --all
